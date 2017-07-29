8ba1ea6345aefccee038a325c742e0f84bc50a9a
xiao qingsong <xiaoqingsong@xiaodeMacBook-Pro.local>
2017-07-29 23:19:37 +0800

    update

--- app/src/main/java/com/xqs/viewpagerdemo/MainActivity.java
+++ app/src/main/java/com/xqs/viewpagerdemo/MainActivity.java
@@ -7,7 +7,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.xqs.viewpagerdemo.fragment.AdFragment;
@@ -19,7 +22,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ViewPager viewPager;
+    public ViewPager viewPager;
+    public Button mSwitch;
 
     List<ImageView> lists = new ArrayList<>();
 
@@ -27,10 +31,14 @@ public class MainActivity extends AppCompatActivity {
 
     int currentIndex = 0;
 
+    int gender = 1;
+
+    public ImageView view1;
+    public ImageView view2;
+    public ImageView view3;
+
     int[] boys = new int[]{R.mipmap.boy01, R.mipmap.boy02, R.mipmap.boy03};
     int[] girls = new int[]{R.mipmap.girl01, R.mipmap.girl02, R.mipmap.girl03};
-
-
 
     ImageHandler mHandler = new ImageHandler(new WeakReference<>(this));
 
@@ -40,19 +48,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LayoutInflater inflater = LayoutInflater.from(this);
-        ImageView view1 = (ImageView) inflater.inflate(R.layout.item_image, null);
-        ImageView view2 = (ImageView) inflater.inflate(R.layout.item_image, null);
-        ImageView view3 = (ImageView) inflater.inflate(R.layout.item_image, null);
+        view1 = (ImageView) inflater.inflate(R.layout.item_image, null);
+        view2 = (ImageView) inflater.inflate(R.layout.item_image, null);
+        view3 = (ImageView) inflater.inflate(R.layout.item_image, null);
 
-        view1.setImageResource(boys[0]);
-        view2.setImageResource(boys[1]);
-        view3.setImageResource(boys[2]);
-
-        lists.add(view1);
-        lists.add(view2);
-        lists.add(view3);
+        lists.addAll(initImageData(gender));
 
         viewPager = (ViewPager) findViewById(R.id.vp);
+        mSwitch = (Button) findViewById(R.id.btn_switch);
 
         imageAdapter = new ImageAdapter(lists);
 
@@ -60,7 +63,46 @@ public class MainActivity extends AppCompatActivity {
 
         mHandler.sendMessageDelayed(new Message(), 2000);
 
+        mSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("gender","gender-->"+gender);
+                if(gender == 1){
+                    switchImage(1);
+                    gender = 2;
+                }else{
+                    switchImage(2);
+                    gender = 1;
+                }
+            }
+        });
+
     }
+
+    public void switchImage(int gender){
+        lists.clear();
+        lists.addAll(initImageData(gender));
+        imageAdapter.notifyDataSetChanged();
+    }
+
+    public List<ImageView> initImageData(int gender){
+        if(gender == 1){
+            view1.setImageResource(boys[0]);
+            view2.setImageResource(boys[1]);
+            view3.setImageResource(boys[2]);
+        }else{
+            view1.setImageResource(girls[0]);
+            view2.setImageResource(girls[1]);
+            view3.setImageResource(girls[2]);
+        }
+        List<ImageView> lists = new ArrayList<>();
+
+        lists.add(view1);
+        lists.add(view2);
+        lists.add(view3);
+        return lists;
+    }
+
 
 
     public static class ImageHandler extends Handler{
